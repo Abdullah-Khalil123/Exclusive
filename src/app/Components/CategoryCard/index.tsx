a66bad2ccf21f5b3b5fa7d00cc82bab11be64314
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-14 23:54:23 +0500

    first commit

---
+++ src/app/Components/CategoryCard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./categorycard.module.css";
+
+const CategoryCard = () => {
+  return <div>CategoryCard</div>;
+};
+
+export default CategoryCard;
---
+++ src/app/Components/ProductCard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./productcard.module.css";
+
+const ProductCard = () => {
+  return <div>ProductCard</div>;
+};
+
+export default ProductCard;
