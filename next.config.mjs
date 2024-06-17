f4faf3d12d0592677ae88c3881a844c2251fdeb0
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-17 18:15:29 +0500

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  async rewrites() {
-    return [
-      {
-        source: "/About",
-        destination: "/Pages/About",
-      },
-      {
-        source: "/SignUp",
-        destination: "/Pages/SignUp",
-      },
-      {
-        source: "/Login",
-        destination: "/Pages/Login",
-      },
-      {
-        source: "/Product/:id",
-        destination: "/Pages/Product/:id",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/About",
+  //       destination: "/Pages/About",
+  //     },
+  //     {
+  //       source: "/SignUp",
+  //       destination: "/Pages/SignUp",
+  //     },
+  //     {
+  //       source: "/Login",
+  //       destination: "/Pages/Login",
+  //     },
+  //     {
+  //       source: "/Product/:id",
+  //       destination: "/Pages/Product/:id",
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
