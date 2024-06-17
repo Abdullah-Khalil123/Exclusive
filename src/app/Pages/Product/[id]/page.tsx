22bd403c56d1065be16b4612bea377967404ab51
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-17 18:29:44 +0500

    work plis

--- src/app/Pages/Product/[id]/page.tsx
+++ src/app/Pages/Product/[id]/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import React from "react";
 
+export const dynamicParams = true;
+export async function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }];
+}
+
 const ProductItem: React.FC<{ params: { id: string } }> = ({ params }) => {
   return <div>ProductItem: {params.id}</div>;
 };
