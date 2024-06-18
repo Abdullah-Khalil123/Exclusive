9f18132a68f13264c030e3978b7d66eb41717fc9
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-18 17:55:08 +0500

    s

--- src/app/Pages/Product/[id]/page.tsx
+++ src/app/Pages/Product/[id]/page.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 export const dynamicParams = true;
-// export async function generateStaticParams() {
-//   return [{ id: "1" }, { id: "2" }];
-// }
-
-export async function getStaticProps({ params }: any) {
-  const product = { id: params.id };
-  return {
-    props: {
-      product,
-    },
-  };
+export async function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }];
 }
+
+// export async function getStaticProps({ params }: any) {
+//   const product = { id: params.id };
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// }
 
 const ProductItem: React.FC<{ params: { id: string } }> = ({ params }) => {
   return <div>ProductItem: {params.id}</div>;
