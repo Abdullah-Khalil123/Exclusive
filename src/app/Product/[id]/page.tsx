1eef818e1c2d863b9c7e57d84b2b463e0bbf9fe9
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-18 20:27:41 +0500

    ne23123

--- src/app/Product/[id]/page.tsx
+++ src/app/Product/[id]/page.tsx
@@ -2,10 +2,21 @@
 import React from "react";
 import { usePathname } from "next/navigation";
 
-const ProductItem = () => {
+const ProductItem = ({ posts }: { posts: any }) => {
   const pathName = usePathname();
   const id = pathName.split("/");
   return <div>ProductItem: {id[2]}</div>;
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    params: {
+      posts,
+    },
+  };
+}
 
 export default ProductItem;
