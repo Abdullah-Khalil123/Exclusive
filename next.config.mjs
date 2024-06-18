62ff0a290331217cfbafa6267c301f3110a1e835
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-18 19:01:25 +0500

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/Exclusive",
-  // output: "export",
+  output: "export",
   images: { unoptimized: false },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/About",
-  //       destination: "/Pages/About",
-  //     },
-  //     {
-  //       source: "/SignUp",
-  //       destination: "/Pages/SignUp",
-  //     },
-  //     {
-  //       source: "/Login",
-  //       destination: "/Pages/Login",
-  //     },
-  //     {
-  //       source: "/Product/:id",
-  //       destination: "/Pages/Product/:id",
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
--- src/app/Product/[id]/page.tsx
+++ src/app/Product/[id]/page.tsx
@@ -3,5 +3,14 @@ import React from "react";
 const ProductItem = ({ params }: { params: { id: string } }) => {
   return <div>ProductItem : {params.id}</div>;
 };
+export async function generateStaticParams() {
+  const products = [{ id: "1" }, { id: "2" }, { id: "3" }];
+
+  return products.map((product) => ({
+    params: {
+      id: product.id,
+    },
+  }));
+}
 
 export default ProductItem;
