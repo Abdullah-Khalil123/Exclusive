68e556b531d453f97ff01438669402490c06911e
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-18 17:34:50 +0500

    work

--- src/app/Pages/Product/[id]/page.tsx
+++ src/app/Pages/Product/[id]/page.tsx
@@ -5,14 +5,14 @@ export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }];
 }
 
-export async function getStaticProps({ params }: any) {
-  const product = { id: params.id };
-  return {
-    props: {
-      product,
-    },
-  };
-}
+// export async function getStaticProps({ params }: any) {
+//   const product = { id: params.id };
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// }
 
 const ProductItem: React.FC<{ params: { id: string } }> = ({ params }) => {
   return <div>ProductItem: {params.id}</div>;
