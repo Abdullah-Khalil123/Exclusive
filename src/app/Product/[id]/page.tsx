86ce22a4dc22991699575b681ee4f05fc771ec60
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-06-19 18:42:54 +0500

    fix on local

--- src/app/Product/[id]/page.tsx
+++ src/app/Product/[id]/page.tsx
@@ -1,59 +1,7 @@
 import React from "react";
-import { GetStaticProps, GetStaticPaths } from "next";
-import { useRouter } from "next/router";
 
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
-
-interface ProductItemProps {
-  post: Post;
-}
-
-const ProductItem = ({ post }: ProductItemProps) => {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = await response.json();
-
-  const paths = posts.map((post: Post) => ({
-    params: { id: post.id.toString() },
-  }));
-
-  return {
-    paths,
-    fallback: true, // See the "fallback" section below
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { id } = context.params!;
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
-  );
-  const post = await response.json();
-
-  return {
-    props: {
-      post,
-    },
-    revalidate: 10, // In seconds
-  };
+const ProductItem = ({ params }: { params: { id: string } }) => {
+  return <div>ProductItem : {params.id}</div>;
 };
 
 export default ProductItem;
